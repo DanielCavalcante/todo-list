43a3cdcc1294cba4c0b0ab19f9b404d5ed9be43f
Daniel Cavalcante <danielcdesouza@hotmail.com>
2015-10-07 20:17:47 -0300

    Style and JS

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require application/modernizr
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree ./application
---
+++ app/assets/javascripts/application/modernizr.browser.js
@@ -0,0 +1,5 @@
+Modernizr.addTest("firefox", navigator.userAgent.match(/Firefox/));
+Modernizr.addTest("chrome", navigator.userAgent.match(/Chrome/));
+Modernizr.addTest("safari", navigator.userAgent.match(/Safari/));
+Modernizr.addTest("ie", navigator.userAgent.match(/MSIE/));
+Modernizr.addTest("opera", navigator.userAgent.match(/Opera/));
